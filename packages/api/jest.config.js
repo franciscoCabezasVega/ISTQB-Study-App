f252e97b3c5ca61aa054b895954567d086a152bb
Francisco Cabezas <frank_vega25@hotmail.com>
2026-01-02 20:38:28 -0500

    fix(tests): add TypeScript configuration for Jest tests
    - Create tsconfig.spec.json with Jest types
    - Add jest.d.ts type reference file
    - Enable isolatedModules to prevent warnings
    - Exclude test files from main tsconfig
    - All 68 tests passing"

--- packages/api/jest.config.js
+++ packages/api/jest.config.js
@@ -3,16 +3,21 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.ts', '**/*.spec.ts', '**/*.test.ts'],
-  testPathIgnorePatterns: ['/node_modules/', '/__tests__/setup.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/__tests__/setup.js', '/__tests__/jest.d.ts'],
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/**/*.d.ts',
     '!src/index.ts',
   ],
   coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov', 'html'],
+  coverageReporters: ['text', 'lcov', 'html', 'json-summary'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
-  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.js'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      tsconfig: './tsconfig.spec.json'
+    }]
+  }
 };
